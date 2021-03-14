3d085a682da9cc7d2730e7c22aa93771256ff8d7
Nick Hernandez <xnickhernandez@gmail.com>
2021-03-13 22:41:07 -0800

    Completed challenge

---
+++ Arcade/intro/arrayReplace.js
@@ -0,0 +1,9 @@
+// Given an array of integers, replace all the occurrences of
+// elemToReplace with substitutionElem.
+
+function arrayReplace(inputArray, elemToReplace, substitutionElem) {
+     for(let i = 0; i < inputArray.length; i++){
+         if(inputArray[i] === elemToReplace) inputArray[i] = substitutionElem;
+     }
+     return inputArray;
+}
