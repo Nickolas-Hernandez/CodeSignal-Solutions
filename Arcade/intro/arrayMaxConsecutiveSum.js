19e8ceed1a6713fc6a3e619b53a7436bc6fe0b1a
Nick Hernandez <xnickhernandez@gmail.com>
2021-04-28 15:45:49 -0700

    Added problem details.

--- Arcade/intro/arrayMaxConsecutiveSum.js
+++ Arcade/intro/arrayMaxConsecutiveSum.js
@@ -1,3 +1,9 @@
+// Given array of integers, find the maximal possible sum of some of its
+// k consecutive elements.
+
+// For inputArray = [2, 3, 5, 1, 6] and k = 2, the output should be
+// arrayMaxConsecutiveSum(inputArray, k) = 8.
+
 function arrayMaxConsecutiveSum(inputArray, k) {
   let currentMax = 0;
   let totalMax = 0;
